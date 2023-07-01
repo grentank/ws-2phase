1090e11978db34f43254416dd29f61269392c4a1
grentank <mexmat100394@gmail.com>
2023-07-01 22:27:36 +0300

    began ws coding

--- src/components/pages/chat/ChatPage.jsx
+++ src/components/pages/chat/ChatPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import UsersList from './ui/UsersList';
 import ChatComponent from './ui/ChatComponent';
@@ -8,6 +8,39 @@ const initUsers = [{ name: 'Alex' }, { name: 'Bob' }, { name: 'Carl' }];
 export default function ChatPage({ messages: initMessages, user: loggedUser }) {
   const [messages, setMessages] = useState(initMessages);
   const [users, setUsers] = useState(initUsers);
+  const socket = useRef(null);
+
+  useEffect(() => {
+    socket.current = new WebSocket('ws://localhost:3000');
+
+    socket.current.onopen = () => {
+      socket.send(JSON.stringify({ type: 'INIT' }));
+    };
+
+    socket.current.onclose = () => {
+      console.log('Socket closed');
+    };
+
+    socket.current.onerror = (err) => {
+      console.log('Socket error', err);
+    };
+
+    socket.current.onmessage = (event) => {
+      const { type, payload } = JSON.parse(event.data);
+
+      switch (type) {
+        case 'INIT':
+          setUsers(payload.users);
+          break;
+        case 'NEW_MESSAGE':
+          setMessages((prevMessages) => [...prevMessages, payload.message]);
+          break;
+        default:
+          console.log('Unknown message type', type);
+      }
+    };
+  }, []);
+
   return (
     <Container>
       <Row>
